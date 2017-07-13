696631bfebb7ecbd8d45398e8a5ea02b18ded523
Joey Mann <joey@skymann.net>
2017-07-13 15:39:17 +1000

    Adds jQuery. Loads text files into variables as arrays.

--- signal.js
+++ signal.js
@@ -1,5 +1,27 @@
-chrome.tabs.onUpdated.addListener(function (tab) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      var currentTab = tabs[0]
-    });
-});
+$(function(){
+          $.get('trust.txt', function(data){
+              var trust = data.split('\n');
+              console.log(trust);
+          });
+      });
+
+$(function(){
+          $.get('satire.txt', function(data){
+              var satire = data.split('\n');
+              console.log(satire);
+          });
+      });
+
+      $(function(){
+          $.get('salt.txt', function(data){
+              var salt = data.split('\n');
+              console.log(salt);
+          });
+      });
+
+$(function(){
+          $.get('false.txt', function(data){
+              var fake = data.split('\n');
+              console.log(fake);
+          });
+      });
