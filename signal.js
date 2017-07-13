02f546a82e3991c39cdaf52e7b49b840bf729ce2
Joey Mann <joey@skymann.net>
2017-07-13 23:03:16 +1000

    Previous commit was garbage version of this one.

--- signal.js
+++ signal.js
@@ -1,24 +1,28 @@
 $(function(){
           $.get('trust.txt', function trustArray(data){
               var trust = data.split('\n');
+              console.log("Signal: " + trust.length + " trusted sites loaded");
           });
       });
 
 $(function(){
-          $.get('satire.txt', function satireray(data){
+          $.get('satire.txt', function satireArray(data){
               var satire = data.split('\n');
+              console.log("Signal: " + satire.length + " satirical sites loaded");
           });
       });
 
       $(function(){
           $.get('salt.txt', function saltArray(data){
               var salt = data.split('\n');
+              console.log("Signal: " + salt.length + " salty sites loaded");
           });
       });
 
 $(function(){
           $.get('false.txt', function falseArray(data){
               var fake = data.split('\n');
+              console.log("Signal: " + fake.length + " false sites loaded");
           });
       });
 
@@ -27,13 +31,13 @@ $(function(){
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
     var currentTab = tabs[0].url
-    console.log(currentTab);
+    console.log("Signal: " + currentTab);
     });
 }); 
 
 chrome.tabs.onActivated.addListener(function(tabId, changeInfo, tab) {
    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
     var currentTab = tabs[0].url
-    console.log(currentTab);
+    console.log("Signal: " + currentTab);
     });
 }); 
