eb427ab1e54ea38f2ea36b6deed99e324f0e36ff
Joey Mann <joey@skymann.net>
2017-07-20 16:51:33 +1000

    Updated lists to make them usable without wildcards. For the time being, sites that use "www" as a subdomain will have those subdomains listed until a solution for wildcards can be determined.

--- signal.js
+++ signal.js
@@ -10,35 +10,32 @@ var Signal = {
 $(function(){
           $.get('trust.txt', function trustArray(data){
               Signal.trust = data.split('\n');
-              console.log("Signal: " + Signal.trust.length + " trusted sites loaded");
+              // console.log("Signal: " + Signal.trust.length + " trusted sites loaded");
           });
       });
 
 $(function(){
           $.get('satire.txt', function satireArray(data){
               Signal.satire = data.split('\n');
-              console.log("Signal: " + Signal.satire.length + " satirical sites loaded");
+              // console.log("Signal: " + Signal.satire.length + " satirical sites loaded");
           });
       });
 
       $(function(){
           $.get('salt.txt', function saltArray(data){
             Signal.salt = data.split('\n');
-            console.log("Signal: " + Signal.salt.length + " salty sites loaded");
+            // console.log("Signal: " + Signal.salt.length + " salty sites loaded");
           });
       });
 
 $(function(){
           $.get('false.txt', function falseArray(data){
               Signal.fake = data.split('\n');
-              console.log("Signal: " + Signal.fake.length + " false sites loaded");
+              // console.log("Signal: " + Signal.fake.length + " false sites loaded");
           });
       });
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-   chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-    Signal.currentTab = tabs[0].url.split("/")[2];
-    function runSignalTest() {
+function runSignalTest() {
         if(Signal.trust.indexOf(Signal.currentTab) !== -1) {
             chrome.browserAction.setIcon({path:"images/trust128.png"});
         } else if(Signal.satire.indexOf(Signal.currentTab) !== -1) {
@@ -51,7 +48,11 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
             chrome.browserAction.setIcon({path:"images/idle128.png"});
         }
     };
-    console.log("Signal: " + Signal.currentTab);
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+   chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+    Signal.currentTab = tabs[0].url.split("/")[2];
+    // console.log("Signal: " + Signal.currentTab);
     runSignalTest();
     });
     
@@ -60,20 +61,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 chrome.tabs.onActivated.addListener(function(tabId, changeInfo, tab) {
    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
     Signal.currentTab = tabs[0].url.split("/")[2];
-    console.log("Signal: " + Signal.currentTab);
-    function runSignalTest() {
-        if(Signal.trust.indexOf(Signal.currentTab) !== -1) {
-            chrome.browserAction.setIcon({path:"images/trust128.png"});
-        } else if(Signal.satire.indexOf(Signal.currentTab) !== -1) {
-            chrome.browserAction.setIcon({path:"images/satire128.png"});
-        } else if(Signal.salt.indexOf(Signal.currentTab) !== -1) {
-            chrome.browserAction.setIcon({path:"images/salt128.png"});
-        } else if(Signal.fake.indexOf(Signal.currentTab) !== -1) {
-            chrome.browserAction.setIcon({path:"images/false128.png"});
-        } else {
-            chrome.browserAction.setIcon({path:"images/idle128.png"});
-        }
-    };
+    // console.log("Signal: " + Signal.currentTab);
     runSignalTest();
     });
 });
